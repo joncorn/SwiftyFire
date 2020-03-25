ff09b3df7c8232fdaa36eb47ca0f2fc2258a092b
Jon <jdcorn@live.com>
2020-03-25 13:44:55 -0600

    implement firestore add, merge, ref id

--- SwiftyFire/Controllers/ViewControllers/RootViewController.swift
+++ SwiftyFire/Controllers/ViewControllers/RootViewController.swift
@@ -15,6 +15,7 @@ class RootViewController: UIViewController {
   // MARK: - View Elements
   lazy var addButton: PurpleButton = {
     let button = PurpleButton(title: "Add")
+    button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     return button
   }()
   
@@ -28,7 +29,7 @@ class RootViewController: UIViewController {
     setupAddButton()
   }
   
-  // MARK: - Setup Methods
+  // MARK: - Setup Views
   func setupViews() {
     view.addSubview(addButton)
   }
@@ -38,4 +39,72 @@ class RootViewController: UIViewController {
     addButton.height(50)
   }
   
+  // MARK: - Methods
+  @objc fileprivate func buttonTapped() {
+    print("Button Pressed")
+    
+    // MARK:  Add data
+    // accessing root directory in firestore database
+    FirestoreReferenceManager.root
+      .collection("cities")
+      .document("LA")
+      .setData(["name": "Los Angeles",
+                "state": "CA",
+                "country": "USA"]) { (error) in
+                  if let error = error {
+                    print(error, error.localizedDescription)
+                  }
+                  print("successfully set data")
+    }
+    
+    // MARK:  Merge data
+    let cityData = [
+      "name": "Los Angeles New"
+    ]
+    
+    FirestoreReferenceManager.root
+      .collection("cities")
+      .document("LA")
+      // using the "merge" option with completion will merge instead of replace
+      .setData(cityData, merge: true) { (error) in
+        if let error = error {
+          print(error.localizedDescription)
+        }
+        print("Successfully merged new data")
+    }
+    
+    // MARK:  Add document
+    let newCityData = [
+      "name": "Tokyo",
+      "country": "Japan"
+    ]
+    
+    // Adding new data to our "city" collection
+    FirestoreReferenceManager.root
+      .collection("cities")
+      .addDocument(data: newCityData) { (error) in
+      if let error = error {
+        print(error.localizedDescription)
+      }
+      print("Successfully set new city data")
+    }
+    
+    // Create a document reference with an auto-generated ID, then use the reference later
+    // MARK:  DocumentID reference
+    let ref = FirestoreReferenceManager.root.collection("cities").document()
+    let documentID = ref.documentID
+    
+    let newestCityData = [
+      "name": "Berlin",
+      "country": "Germany",
+      "uid": documentID
+    ]
+    
+    ref.setData(newestCityData, merge: true) { (error) in
+      if let error = error {
+        print(error.localizedDescription)
+      }
+      print("Successfully set newest city data")
+    }
+  }
 }
